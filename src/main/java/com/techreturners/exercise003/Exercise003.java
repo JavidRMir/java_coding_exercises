42f7f15bcd2b3559250ed1ddac31085779678849
Javid Mir <mir.javid@gmail.com>
2021-09-02 20:44:59 +0100

    Completed Exercise003Test

--- src/main/java/com/techreturners/exercise003/Exercise003.java
+++ src/main/java/com/techreturners/exercise003/Exercise003.java
@@ -1,13 +1,46 @@
 package com.techreturners.exercise003;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Exercise003 {
 
+    String[] iceCreamFlavoursArray = {
+            "Pistachio",
+            "Raspberry Ripple",
+            "Vanilla",
+            "Mint Chocolate Chip",
+            "Chocolate",
+            "Mango Sorbet"
+    };
+
+//    List<String> iceCreamFlavoursList = Arrays.asList(
+//            "Pistachio",
+//            "Raspberry Ripple",
+//            "Vanilla",
+//            "Mint Chocolate Chip",
+//            "Chocolate",
+//            "Mango Sorbet");
+
+    List<String> iceCreamFlavoursList = Arrays.asList(iceCreamFlavoursArray);
+
     int getIceCreamCode(String iceCreamFlavour) {
-        throw new UnsupportedOperationException(("You can delete this statement and add your code here."));
+
+        int iceCreamCode = iceCreamFlavoursList.indexOf(iceCreamFlavour);
+
+        if (iceCreamCode == -1) {
+            throw new UnsupportedOperationException("Ice cream flavour not found");
+        } else {
+            return iceCreamCode;
+        }
     }
 
     String[] iceCreamFlavours() {
-        throw new UnsupportedOperationException(("You can delete this statement and add your code here."));
+        if (iceCreamFlavoursArray.length > 0) {
+            return iceCreamFlavoursArray;
+        } else {
+            throw new UnsupportedOperationException(("No Ice cream flavours found"));
+        }
     }
 
 }
