04162c49f26481f6596adde4e6b64c6fd6f870c1
Javid Mir <mir.javid@gmail.com>
2021-09-04 16:58:56 +0100

    Completed Exercise005Test

--- src/main/java/com/techreturners/exercise005/Exercise005.java
+++ src/main/java/com/techreturners/exercise005/Exercise005.java
@@ -3,7 +3,17 @@ package com.techreturners.exercise005;
 public class Exercise005 {
 
     public boolean isPangram(String input) {
-        throw new UnsupportedOperationException("You can delete this statement and add your code here.");
+        input = input.toLowerCase();
+        if(input.length() < 26) {
+            return false;
+        } else {
+            for(char ch = 'a'; ch <= 'z'; ch++) {
+                if(input.indexOf(ch) == -1) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
 }
